84b423606f0bc4d7e9dc8e90f7e34c9ea12b35b8
robson <montenegrodr@gmail.com>
2017-08-05 13:17:12 +0100

    - Bumping version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.rst", "rb") as f:
     long_descr = f.read().decode("utf-8")
     
 setup(name='daftlistings',
-      version='1.1.4',
+      version='1.1.5',
       description='A library that enables programmatic interaction with daft.ie. ',
       long_description=long_descr,
       url='https://github.com/AnthonyBloomer/daftlistings',
